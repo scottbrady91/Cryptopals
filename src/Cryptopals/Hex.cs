b2f74292f47aa3fdfcf5d09708d288b009234ea6
Scott Brady <scott@scottbrady91.com>
2018-12-15 09:59:24 +0000

    Hex string handling

---
+++ src/Cryptopals/Hex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cryptopals
+{
+    public static class Hex
+    {
+        // hex = base16 (0-9 and A-F (or a-f))
+        // each digit represents 4 bits (a nibble)
+        // 0000 0000 = 00 && 1111 1111 = FF
+        // prefixed by 0x in C languages (e.g. 0xFF)
+        public static byte[] StringToBytes(string hex)
+        {
+            var hexAsBytes = new byte[hex.Length / 2];
+
+            for (var i = 0; i < hex.Length; i += 2)
+            {
+                hexAsBytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            
+            return hexAsBytes;
+        }
+
+        public static string BytesToString(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes)
+                .Replace("-", string.Empty).ToLower(); // BitConverter defaults to format of 4D-61-6e
+        }
+    }
+}
---
+++ test/Cryptopals.Test/Functionality/HexTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Cryptopals.Test.Functionality
+{
+    public class HexTests
+    {
+        [Fact]
+        public void BytesToString()
+        {
+            var testBytes = new[] {Convert.ToByte(77), Convert.ToByte(97), Convert.ToByte(110)};
+            const string expected = "4d616e";
+
+            Hex.BytesToString(testBytes).Should().Be(expected);
+        }
+
+        [Fact]
+        public void StringToBytes()
+        {
+            const string testString = "4d616e";
+            var expected = new[] { Convert.ToByte(77), Convert.ToByte(97), Convert.ToByte(110) };
+
+            Hex.StringToBytes(testString).Should().BeEquivalentTo(expected);
+        }
+    }
+}
