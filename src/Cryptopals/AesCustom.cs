2fdc38b1e2eabcbb1b605079197dc909f35f5400
Scott Brady <scott@scottbrady91.com>
2022-12-18 10:15:18 +0000

    Refactor of AES wrapper/implementation. Started challenge 10

---
+++ src/Cryptopals/AesCustom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Cryptopals;
+
+public class AesCustom
+{
+    private Aes aes;
+    
+    private AesCustom() { }
+    
+    public static AesCustom Create(byte[] key)
+    {
+        var aes = Aes.Create();
+        aes.Key = key;
+
+        return new AesCustom { aes = aes };
+    }
+    
+    public IEnumerable<byte> DecryptCbc(Span<byte> ciphertext)
+    {
+        var plaintext = new List<byte>();
+        for (var i = 0; i < ciphertext.Length / 16; i++)
+        {
+            // TODO: XOR decrypted plaintext block against next ciphertext block
+            
+            plaintext.AddRange(aes.DecryptEcb(ciphertext.Slice(i * 16, 16), PaddingMode.None));
+        }
+        
+        return plaintext;
+    }
+
+    public IEnumerable<byte> DecryptEcb(Span<byte> ciphertext)
+    {
+        var plaintext = new List<byte>();
+        for (var i = 0; i < ciphertext.Length / 16; i++)
+        {
+            plaintext.AddRange(aes.DecryptEcb(ciphertext.Slice(i * 16, 16), PaddingMode.None));
+        }
+        
+        return plaintext;
+    }
+}
--- test/Cryptopals.Test/Set2.cs
+++ test/Cryptopals.Test/Set2.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 using FluentAssertions;
 using Xunit;
 
@@ -7,7 +10,7 @@ namespace Cryptopals.Test;
 public class Set2
 {
     [Fact]
-    public void Challenge1() // Implement PKCS#7 padding
+    public void Challenge9() // Implement PKCS#7 padding
     {
         const string block = "YELLOW SUBMARINE";
         const string expectedPaddedBlock = "YELLOW SUBMARINE\x04\x04\x04\x04"; // padded to 20 bytes
@@ -15,5 +18,18 @@ public class Set2
         var paddedBytes = Pkcs7.Pad(Encoding.ASCII.GetBytes(block), 20);
 
         Encoding.ASCII.GetString(paddedBytes).Should().Be(expectedPaddedBlock);
-    } 
+    }
+
+    [Fact]
+    public void Challenge10() // Implement CBC mode
+    {
+        const string key = "YELLOW SUBMARINE";
+        var ciphertext = Convert.FromBase64String(File.ReadAllText("TestData/Set2/10.txt"));
+
+        var aes = AesCustom.Create(Encoding.ASCII.GetBytes(key));
+        var plaintextBytes = aes.DecryptCbc(ciphertext).ToArray();
+
+        plaintextBytes.Should().NotBeNullOrEmpty();
+        var plaintext = Encoding.ASCII.GetString(plaintextBytes);
+    }
 }
