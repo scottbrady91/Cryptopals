b6c792cba1f8823307756e6b15ec3415f0489a7b
Scott Brady <scott@scottbrady91.com>
2025-02-02 13:42:25 +0000

    Uncommitted changes

--- test/Cryptopals.Test/Functionality/HexTests.cs
+++ test/Cryptopals.Test/Functionality/HexTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using FluentAssertions;
 using Xunit;
 
@@ -7,7 +8,7 @@ namespace Cryptopals.Test.Functionality
     public class HexTests
     {
         [Fact]
-        public void BytesToString()
+        public void BytesToString_Custom()
         {
             var testBytes = new[] {Convert.ToByte(77), Convert.ToByte(97), Convert.ToByte(110)};
             const string expected = "4d616e";
@@ -16,10 +17,27 @@ namespace Cryptopals.Test.Functionality
         }
 
         [Fact]
-        public void StringToBytes()
+        public void StringToBytes_Custom()
         {
             const string testString = "4d616e";
             var expected = new[] { Convert.ToByte(77), Convert.ToByte(97), Convert.ToByte(110) };
+
+            Hex.StringToBytes(testString).Should().BeEquivalentTo(expected);
+        }
+        [Fact]
+        public void BytesToString_Dotnet()
+        {
+            var testBytes = RandomNumberGenerator.GetBytes(32);
+            var expected = Convert.ToHexString(testBytes);
+
+            Hex.BytesToString(testBytes).Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void StringToBytes_Dotnet()
+        {
+            var expected = RandomNumberGenerator.GetBytes(32);
+            var testString = Convert.ToHexString(expected);
 
             Hex.StringToBytes(testString).Should().BeEquivalentTo(expected);
         }
